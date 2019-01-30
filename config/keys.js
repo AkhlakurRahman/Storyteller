87b663c47af5ecd9e4fa1e2cecdb109a9f30be84
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-01-30 18:10:42 +0600

    Google Oauth authentication added

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb://story:story1@ds143474.mlab.com:43474/storyteller-dev',
+	googleClientID: '249981907752-to1ljffa7u8evk3hfifcfo1vas9kkhne.apps.googleusercontent.com',
+	googleClientSecret: 'WjQ9QLAnigj85QTVWl_1s1NC'
+};
