73584b830a60cb489dccb8f4e994226766f46a4d
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-01-31 02:20:17 +0600

    Welcome page and sidebar added

--- app.js
+++ app.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// Load models
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Load routers
 const auth = require('./routes/auth');
+const index = require('./routes/index');
+
+// Load keys
+const keys = require('./config/keys');
+
+// Map global promise
+// mongoose.Promise = global.Promise;
+
+// Connect to mongoDB
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser: true
+}).then(() => {
+	console.log('Connected to mongoDB database!');
+}).catch(err => {
+	console.log(err);
+});
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Wow');
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Setting global variables
+app.use((req, res, next) => {
+	res.locals.user = req.user || null;
+	next();
 });
 
+app.use('/', index);
 app.use('/auth', auth);
 
 const port = process.env.PORT || 3000;
--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@ const GoogleStrategy = require('passport-google-oauth20');
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load User Model
+const User = mongoose.model('users');
+
 module.exports = (passport) => {
 	passport.use(new GoogleStrategy({
 		clientID: keys.googleClientID,
@@ -9,9 +12,44 @@ module.exports = (passport) => {
 		callbackURL: '/auth/google/callback',
 		proxy: true
 	}, (accessToken, refreshToken, profile, done) => {
-			console.log(accessToken);
-			console.log(profile);
+			/*console.log(accessToken);
+			console.log(profile);*/
+
+			const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+
+			const newUser = {
+				googleID: profile.id,
+				firstName: profile.name.givenName,
+				lastName: profile.name.familyName,
+				email: profile.emails[0].value,
+				image
+			};
+
+			// Check for existing user
+			User.findOne({
+				googleID: profile.id
+			}).then(user => {
+				if (user) {
+					// Return exiting user
+					done(null, user);
+				} else {
+					// Add new user
+					new User(newUser)
+						.save()
+						.then(user => {
+							done(null, user);
+						});
+				}
+			});
 		}
 
 	));
+
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser((id, done) => {
+		User.findById(id).then(user => done(null, user));
+	});
 };
